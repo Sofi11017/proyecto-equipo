c67153bb7d52a961eb5f2260a74e34f9a095ad38
Sofia Salazar <sof71101@gmail.com>
2026-04-29 15:51:36 -0500

    estructura inicial del proyecto

---
+++ funcion.py
@@ -0,0 +1,8 @@
+def sumar_dos_numeros():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    suma = num1 + num2
+    print(f"La suma de {num1} y {num2} es: {suma}")
+
+# Llamar la función
+sumar_dos_numeros()
